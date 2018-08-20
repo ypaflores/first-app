17293cba94d1e1b32fb4710bd37949180b39d0bd
gyparraguirre <studente.giovany.flores@gmail.com>
2018-08-20 21:28:26 +0200

    traducido casi todas las paginas

--- src/pages/insert-noticia/insert-noticia.module.ts
+++ src/pages/insert-noticia/insert-noticia.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InsertNoticiaPage } from './insert-noticia';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { InsertNoticiaPage } from './insert-noticia';
   ],
   imports: [
     IonicPageModule.forChild(InsertNoticiaPage),
+    TranslateModule.forChild()
   ],
 })
 export class InsertNoticiaPageModule {}
